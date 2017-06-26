356b47894e570c6df26f3f5f00c634f3869f1329
Devon Moubry <devon@moubry.com>
2017-06-26 18:04:13 -0500

    Add styles and features to carousel

--- carousel_slick/js/main.js
+++ carousel_slick/js/main.js
@@ -31,4 +31,10 @@ $('.work').on('sticky-end', function() {
     $('.email-text').remove();
 });
 
-$('.slides').slick();
+$('.slides').slick({
+  fade: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  dots: true
+});
